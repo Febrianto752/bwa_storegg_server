a68ae21b1d5ba97e577d3ff5987da019c3ade417
Febrianto752 <febrianto.bekasi@gmail.com>
2024-03-26 14:23:40 +0700

    feat: add create category and update view category

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,5 +1,23 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
-    return res.render("admin/category/view_category");
+    const categories = await Category.find();
+    return res.render("admin/category/view_category", { categories });
+  },
+  viewCreate: async (req, res) => {
+    return res.render("admin/category/create");
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({ name });
+      await category.save();
+
+      res.redirect("/category");
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
