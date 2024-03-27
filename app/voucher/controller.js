5bb340c75372773678df514d9b8e007c21063509
Febrianto752 <febrianto.bekasi@gmail.com>
2024-03-27 16:14:54 +0700

    feat: procesing add create voucher feature

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -1,4 +1,9 @@
 const Voucher = require("./model");
+const Category = require("../category/model");
+const Nominal = require("../nominal/model");
+const path = require("path");
+const fs = require("fs");
+const config = require("../../config");
 
 module.exports = {
   index: async (req, res) => {
@@ -12,6 +17,31 @@ module.exports = {
       req.flash("alertMessage", `${error.message}`);
       req.flash("alertStatus", "danger");
       res.redirect("/voucher");
+    }
+  },
+  viewCreate: async (req, res) => {
+    const categories = await Category.find();
+    const nominals = await Nominal.find();
+    return res.render("admin/voucher/create", { categories, nominals });
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name, category, nominals } = req.body;
+
+      if (req.file) {
+        let tmp_path = req.file.path;
+      }
+      // let category = await Category({ name });
+      await category.save();
+
+      req.flash("alertMessage", "Berhasil menambah kategori");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/category");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/category");
     }
   },
 };
