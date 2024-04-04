0d5123d3faeac2a6313b0e392a754e01c68f6f63
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-04 10:54:16 +0700

    feat: add signin api

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { signup } = require("./controller");
+const { signup, signin } = require("./controller");
 const router = express.Router();
 const multer = require("multer");
 const os = require("os");
 
 router.post("/signup", multer({ dest: os.tmpdir() }).single("image"), signup);
 
-router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
