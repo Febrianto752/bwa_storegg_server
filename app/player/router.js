f104c66b196199087be91f60023550bc43d362f9
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-09 16:55:34 +0700

    feat: add history and history detail api

--- app/player/router.js
+++ app/player/router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const { landingPage, detailPage, category, checkout, history, historyDetail } = require("./controller");
 const { isLoginPlayer } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 
 module.exports = router;
