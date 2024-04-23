7fcd8c50db420a2c8a4a49ac17340ee1f2461763
Febrianto752 <febrianto.bekasi@gmail.com>
2024-04-23 08:25:14 +0700

    feat: update detail page api

--- app/player/controller.js
+++ app/player/controller.js
@@ -28,6 +28,7 @@ module.exports = {
         .populate("category")
         .populate("nominals")
         .populate("user", "_id name phoneNumber");
+      const payments = await Payment.find({ status: "Y" }).populate("banks");
 
       if (!voucher) {
         return res
@@ -35,7 +36,12 @@ module.exports = {
           .json({ message: "voucher game tidak ditemukan.!" });
       }
 
-      res.status(200).json({ data: voucher });
+      const data = {
+        ...voucher._doc,
+        payments,
+      };
+
+      res.status(200).json({ data });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
@@ -129,57 +135,56 @@ module.exports = {
   },
   history: async (req, res) => {
     try {
-      const { status = '' } = req.query;
+      const { status = "" } = req.query;
 
-      let criteria = {}
+      let criteria = {};
 
       if (status.length) {
         criteria = {
           ...criteria,
-          status: { $regex: `${status}`, $options: 'i' }
-        }
+          status: { $regex: `${status}`, $options: "i" },
+        };
       }
 
       if (req.player._id) {
         criteria = {
           ...criteria,
-          player: req.player._id
-        }
+          player: req.player._id,
+        };
       }
 
-      const history = await Transaction.find(criteria)
+      const history = await Transaction.find(criteria);
 
       let total = await Transaction.aggregate([
         { $match: criteria },
         {
           $group: {
             _id: null,
-            value: { $sum: "$value" }
-          }
-        }
-      ])
+            value: { $sum: "$value" },
+          },
+        },
+      ]);
 
       res.status(200).json({
         data: history,
-        total: total.length ? total[0].value : 0
-      })
-
+        total: total.length ? total[0].value : 0,
+      });
     } catch (err) {
-      res.status(500).json({ message: err.message || `Internal server error` })
+      res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
   historyDetail: async (req, res) => {
     try {
-      const { id } = req.params
+      const { id } = req.params;
 
-      const history = await Transaction.findOne({ _id: id })
+      const history = await Transaction.findOne({ _id: id });
 
-      if (!history) return res.status(404).json({ message: "history tidak ditemukan." })
+      if (!history)
+        return res.status(404).json({ message: "history tidak ditemukan." });
 
-      res.status(200).json({ data: history })
-
+      res.status(200).json({ data: history });
     } catch (err) {
-      res.status(500).json({ message: err.message || `Internal server error` })
+      res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
   dashboard: async (req, res) => {
@@ -188,34 +193,33 @@ module.exports = {
         { $match: { player: req.player._id } },
         {
           $group: {
-            _id: '$category',
-            valeu: { $sum: '$value' }
-          }
-        }
-      ])
+            _id: "$category",
+            valeu: { $sum: "$value" },
+          },
+        },
+      ]);
 
-      const category = await Category.find({})
+      const category = await Category.find({});
 
-      category.forEach(element => {
-        count.forEach(data => {
+      category.forEach((element) => {
+        count.forEach((data) => {
           if (data._id.toString() === element._id.toString()) {
-            data.name = element.name
+            data.name = element.name;
           }
-        })
+        });
       });
 
       const history = await Transaction.find({ player: req.player._id })
-        .populate('category')
-        .sort({ 'updatedAt': -1 })
+        .populate("category")
+        .sort({ updatedAt: -1 });
 
-      res.status(200).json({ data: history, count: count })
+      res.status(200).json({ data: history, count: count });
     } catch (err) {
-      res.status(500).json({ message: err.message || `Internal server error` })
+      res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
   profile: async (req, res) => {
     try {
-
       const player = {
         id: req.player._id,
         username: req.player.username,
@@ -223,51 +227,59 @@ module.exports = {
         name: req.player.name,
         avatar: req.player.avatar,
         phone_number: req.player.phoneNumber,
-      }
+      };
 
-      res.status(200).json({ data: player })
-
+      res.status(200).json({ data: player });
     } catch (err) {
-      res.status(500).json({ message: err.message || `Internal server error` })
+      res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
   editProfile: async (req, res, next) => {
     try {
-      const { name = "", phoneNumber = "" } = req.body
+      const { name = "", phoneNumber = "" } = req.body;
 
-      const payload = {}
+      const payload = {};
 
-      if (name.length) payload.name = name
-      if (phoneNumber.length) payload.phoneNumber = phoneNumber
+      if (name.length) payload.name = name;
+      if (phoneNumber.length) payload.phoneNumber = phoneNumber;
 
       if (req.file) {
-
         let tmp_path = req.file.path;
-        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
-        let filename = req.file.filename + '.' + originaExt;
-        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+        let originaExt =
+          req.file.originalname.split(".")[
+            req.file.originalname.split(".").length - 1
+          ];
+        let filename = req.file.filename + "." + originaExt;
+        let target_path = path.resolve(
+          config.rootPath,
+          `public/uploads/${filename}`
+        );
 
-        const src = fs.createReadStream(tmp_path)
-        const dest = fs.createWriteStream(target_path)
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
 
-        src.pipe(dest)
+        src.pipe(dest);
 
-        src.on('end', async () => {
-          let player = await Player.findOne({ _id: req.player._id })
+        src.on("end", async () => {
+          let player = await Player.findOne({ _id: req.player._id });
 
           let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
           if (fs.existsSync(currentImage)) {
-            fs.unlinkSync(currentImage)
+            fs.unlinkSync(currentImage);
           }
 
-          player = await Player.findOneAndUpdate({
-            _id: req.player._id
-          }, {
-            ...payload,
-            avatar: filename
-          }, { new: true, runValidators: true })
+          player = await Player.findOneAndUpdate(
+            {
+              _id: req.player._id,
+            },
+            {
+              ...payload,
+              avatar: filename,
+            },
+            { new: true, runValidators: true }
+          );
 
-          console.log(player)
+          console.log(player);
 
           res.status(201).json({
             data: {
@@ -275,18 +287,21 @@ module.exports = {
               name: player.name,
               phoneNumber: player.phoneNumber,
               avatar: player.avatar,
-            }
-          })
-        })
+            },
+          });
+        });
 
-        src.on('err', async () => {
-          next(err)
-        })
-
+        src.on("err", async () => {
+          next(err);
+        });
       } else {
-        const player = await Player.findOneAndUpdate({
-          _id: req.player._id
-        }, payload, { new: true, runValidators: true })
+        const player = await Player.findOneAndUpdate(
+          {
+            _id: req.player._id,
+          },
+          payload,
+          { new: true, runValidators: true }
+        );
 
         res.status(201).json({
           data: {
@@ -294,18 +309,17 @@ module.exports = {
             name: player.name,
             phoneNumber: player.phoneNumber,
             avatar: player.avatar,
-          }
-        })
+          },
+        });
       }
-
     } catch (err) {
       if (err && err.name === "ValidationError") {
         res.status(422).json({
           error: 1,
           message: err.message,
-          fields: err.errors
-        })
+          fields: err.errors,
+        });
       }
     }
-  }
+  },
 };
